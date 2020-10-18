6c6be296da5ed61a1488abde8bf1a11db4be19e9
Dhineshamt <a.dhinesh1@gmail.com>
2020-10-18 18:36:38 +0530

    updating cucumber-html-report.js for reporting

--- cypress/integration/images/images.spec.ts
+++ cypress/integration/images/images.spec.ts
@@ -1,5 +1,4 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
-import { Login } from './../../page_action/login';
 
 Given('I open Google images page', () => {
   cy.visit('/imghp');
--- cypress/report_config/cucumber-html-report.js
+++ cypress/report_config/cucumber-html-report.js
@@ -2,7 +2,7 @@ var reporter = require("cucumber-html-reporter");
 
 var options = {
   theme: "bootstrap",
-  jsonDir: "cypress/reports/cucumber_report.json",
+  jsonDir: "cypress/reports",
   output: "cypress/reports/cucumber_report.html",
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
